3451437ed368f699f775d979b55acb0733d0f53b
KennyDwiCh <70674797+KennyDwiCh@users.noreply.github.com>
2020-11-27 12:13:57 +0700

    Push More stuff

---
+++ Bahan WEB/Auth.js
@@ -0,0 +1,78 @@
+const { json } = require('body-parser');
+const { error } = require('console');
+const express = require('express');
+const session = require('express-session');
+const mongoose = require("mongoose");
+const acc = require('./acc');
+
+mongoose.connect(
+    "mongodb://127.0.0.1:27017/MongoDB",
+    {useNewUrlParser:true}
+)
+const db = mongoose.connection;
+db.once("open",()=>{
+    console.log("Succses connect ke monggose")
+})
+const account = require('./acc')
+const Router = express.Router();
+
+Router.get('/Login', async (req,res)=>{
+    if (req.session.user){
+        res.redirect('/');
+    }else{
+        res.render('/Login', {layout:false});
+    }
+});
+
+Router.post('/Login', async (req,res)=>{
+    
+    const username = req.body.username;
+    const password = req.body.password;
+
+    acc.find({"username": username, "password": password}).exec((error,data)=>{
+        if(error) console.log(JSON.stringify(error));
+        if(data){
+            console.log("Find: "+JSON.stringify(data));
+            req.session.user = username;
+            res.redirect('/');
+        }
+    });
+});
+Router.get('/register',async(req,res)=>{
+    res.render('register',{layout:false});
+});
+
+Router.post('/register',async(req,res)=>{
+    const name = req.body.name;
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const reppass = req.body.reppass;
+    const dateof = req.body.dateof;
+    const gender = req.body.gender;
+    const address = req.body.address;
+    const mobnum = req.body.mobnum;
+    
+    var acc_insert = new acc({
+        name : name,
+        usernam : username,
+        email : email,
+        password : password,
+        reppass : reppass,
+        dateof : dateof,
+        gender : gender,
+        address : address,
+        mobnum : mobnum,
+    })
+    acc_insert.save((err,product) => {
+        if (err) console.log(err);
+        console.log(JSON.stringify(product))
+        res.redirect('/Login');
+    });
+});
+
+Router.get('/logout',async(req,res)=>{
+    req.session.destroy();
+    res.redirect('/Login');
+});
+module.exports = Router;
---
+++ Bahan WEB/index.js
@@ -0,0 +1,37 @@
+const express = require('express'); 
+
+const bodyParser = require('body-parser'); 
+
+ 
+
+const app = express(); 
+
+ 
+
+// set the view engine to ejs 
+
+app.set('view engine', 'ejs'); 
+
+ 
+
+// body-parser to parse request body 
+
+app.use(bodyParser.urlencoded()); 
+
+ 
+
+// static files 
+
+app.use(express.static('public')); 
+
+ 
+
+// routes 
+
+ 
+
+// start server on port 3000 
+
+app.listen(3000); 
+
+console.log('Server runs at port 3000...'); 
