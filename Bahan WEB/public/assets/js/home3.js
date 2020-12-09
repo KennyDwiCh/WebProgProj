987eaff22608fb9da9133b05182860b2d18dff7b
ivnnj <abcdef33yk@gmail.com>
2020-12-10 03:48:44 +0700

    update

--- Bahan WEB/public/assets/js/home3.js
+++ Bahan WEB/public/assets/js/home3.js
@@ -21,6 +21,15 @@ function sendInfo(data){
     localStorage.setItem("serialNumber", imgs);
 }
 
+function basket(){
+  // var imgs = data.className;
+  // localStorage.setItem("serialNumber", imgs);
+}
+
+var addtobasket = document.getElementById("addtobasket");
+var listbasket = document.getElementById("listbasket");
+
+
 var locImage = localStorage.getItem("serialNumber");
 var slicing = locImage.slice(16);
 console.log(locImage);
@@ -32,88 +41,117 @@ window.addEventListener('load', function(){
 
 $.getJSON('Database JSON/kumpulanData/allItem.json', function(data){
   let allItem = data.allItem;
-    $.each(allItem, function(i, data){
-        if(slicing == data.ID){
-          document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
-
-          var valueCount
-          var price = data.ListPrice;
-
-          function priceTotal() {
-              var total = valueCount * price;
-              var totalChange = formatToCurrency(total);
-              document.getElementById("totalChange").innerText = totalChange;
-          }
-
-          document.querySelector(".plus-btn").addEventListener("click", function() {
-              valueCount = document.getElementById("quantity").value;
-              valueCount++;
-              document.getElementById("quantity").value = valueCount;
-
-              if (valueCount > 1) {
-                  document.querySelector(".minus-btn").removeAttribute("disabled");
-                  document.querySelector(".minus-btn").classList.remove("disabled")
-              }
-
-              priceTotal()
-          })
-
-          document.querySelector(".minus-btn").addEventListener("click", function() {
-              valueCount = document.getElementById("quantity").value;
-              valueCount--;
-              document.getElementById("quantity").value = valueCount
-
-              if (valueCount == 1) {
-                  document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
-              }
-
-              priceTotal()
-          })
-        }
-        else{
-          console.log("gk ada");
-        }
-    }); 
   $.each(allItem, function(i, data){
       if(slicing == data.ID){
       
-        var menPack = slicing.slice(0,3);
-        var womenPack = slicing.slice(0,5);
-        var furniturePack = slicing.slice(0,9);
-        var hoodiePack = slicing.slice(0,6);
-        var musicPack = slicing.slice(0,5);
-        var elecPack = slicing.slice(0,4);
-        var fashionPack = slicing.slice(0,7);
-        var tfurniturePack = slicing.slice(0,10);
-        var technewsPack = slicing.slice(0,8);
-        var totebackPack = slicing.slice(0,7); 
-
-        if(data.ID.slice(0,1) == menPack){
+        // var menPack = slicing.slice(0,3);
+        // var womenPack = slicing.slice(0,5);
+        // var furniturePack = slicing.slice(0,9);
+        // var hoodiePack = slicing.slice(0,6);
+        // var musicPack = slicing.slice(0,5);
+        // var elecPack = slicing.slice(0,4);
+        // var fashionPack = slicing.slice(0,7);
+        // var tfurniturePack = slicing.slice(0,10);
+        // var technewsPack = slicing.slice(0,8);
+        // var totebackPack = slicing.slice(0,7); 
           document.getElementById("imgChange").src = data.Image;
           document.getElementById("nameChange").innerHTML = data.Name;
           document.getElementById("priceChange").innerHTML = formatToCurrency(data.ListPrice);
           document.getElementById("descChange").innerHTML = data.Description;
           document.getElementById("totalChange").innerHTML = formatToCurrency(data.ListPrice);;
-          console.log("cowo");
+          
+          if(slicing.slice(0,8)=="technews"){
+            document.getElementById("nameItem").innerHTML = data.nameItem;
+            document.getElementById("screenItem").innerHTML = data.Screen;
+            document.getElementById("wieghtItem").innerHTML = data.Wieght;
+            document.getElementById("processorItem").innerHTML = data.Processor;
+            document.getElementById("osItem").innerHTML = data.OS;
+            document.getElementById("gpuItem").innerHTML = data.GPU;
+            document.getElementById("memoryItem").innerHTML = data.Memory;
+            document.getElementById("storageItem").innerHTML = data.Storage;
+            document.getElementById("audioItem").innerHTML = data.Audio;
+            document.getElementById("baterryItem").innerHTML = data.Battery;
+          }else if(slicing.slice(0,9)=="furniture" || slicing.slice(0,5)=="music" || slicing.slice(0,10)=="tfurniture" || slicing.slice(0,4)=="elec" || slicing=="men4"){
+            document.getElementById("sizeList").remove();
+          }
           return false;
-        }
-        else if(slicing.slice(0,5) == womenPack){
-          document.getElementById("imgChange").src = data.Image;
-          document.getElementById("nameChange").innerHTML = data.Name;
-          document.getElementById("priceChange").innerHTML = formatToCurrency(data.ListPrice);
-          document.getElementById("descChange").innerHTML = data.Description;
-          document.getElementById("totalChange").innerHTML = formatToCurrency(data.ListPrice);;
-          console.log("cewe");
-          return false;
-        }
-        else{
-          console.log("eror");
-        }
       }
       else{
         console.log("gk ada");
+      } 
+  });
+  $.each(allItem, function(i, data){
+    if(slicing == data.ID){
+      document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+
+      var valueCount=1;
+      var price = data.ListPrice;
+
+      function priceTotal() {
+          var total = valueCount * price;
+          var totalChange = formatToCurrency(total);
+          document.getElementById("totalChange").innerText = totalChange;
       }
-  }); 
+
+      document.querySelector(".plus-btn").addEventListener("click", function() {
+          valueCount = document.getElementById("quantity").value;
+          valueCount++;
+          document.getElementById("quantity").value = valueCount;
+
+          if (valueCount > 1) {
+              document.querySelector(".minus-btn").removeAttribute("disabled");
+              document.querySelector(".minus-btn").classList.remove("disabled")
+          }
+
+          priceTotal()
+      })
+
+      document.querySelector(".minus-btn").addEventListener("click", function() {
+          valueCount = document.getElementById("quantity").value;
+          valueCount--;
+          document.getElementById("quantity").value = valueCount
+
+          if (valueCount == 1) {
+              document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+          }
+
+          priceTotal()
+      })
+      x=0;
+      array = Array();
+      addtobasket.addEventListener("click", function(){
+        var title = data.Name;
+        var gambar = data.Image;
+        var harga = data.ListPrice*valueCount;
+        var total = formatToCurrency(harga);
+        
+        var newBasket = document.createElement("tr");
+        var basketItems = document.getElementsByClassName('basketItems')[0]
+
+        var cartRowContents = `
+        
+          <td>
+            <img src="${gambar}" width="60px">
+          </td>
+          <td style="font-size: small ">
+            <span style="display:inline-block; white-space: nowrap; overflow: hidden;text-overflow: ellipsis; max-width: 70px;">${title}</span><br>
+            <span>${valueCount} item(s)</span>
+          </td>
+          <td style="font-size: medium; color: #006667;">${total}</td>
+        
+          `
+
+        listbasket.innerHTML = cartRowContents;
+        // array[x] = listbasket.innerHTML;
+        // basketItems.append(listbasket);
+        // localStorage.setItem("basket", listbasket);
+        // x++;
+      });
+    }
+    else{
+      console.log("gk ada");
+    }
+});  
 });
 
 function imageGallery() {
--- Bahan WEB/public/assets/js/index.js
+++ Bahan WEB/public/assets/js/index.js
@@ -1,9 +1,6 @@
 const express = require('express'); 
 const router = express.Router();
 const bodyParser = require('body-parser'); 
-
- 
-
 const app = express(); 
 
 router.use(express.json());
