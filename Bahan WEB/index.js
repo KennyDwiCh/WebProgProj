5a0ca2f9044fb19e5b3fa28a61eb1f15f49a61cc
KennyDwiCh <70674797+KennyDwiCh@users.noreply.github.com>
2020-12-09 20:34:25 +0700

    Route fix

--- Bahan WEB/index.js
+++ Bahan WEB/index.js
@@ -1,12 +1,15 @@
 const express = require('express'); 
 const router = express.Router();
 const bodyParser = require('body-parser'); 
+const session = require('express-session');
+const mongoose = require("mongoose");
+const acc = require('./models/user');
 
  
 
 const app = express(); 
 
-router.use(express.json());
+app.use(express.json());
 
 // set the view engine to ejs 
 
@@ -16,19 +19,82 @@ app.set('view engine', 'ejs');
 
 // body-parser to parse request body 
 
-app.use(bodyParser.urlencoded()); 
+router.use(bodyParser.urlencoded()); 
 
+mongoose.connect(
+    "mongodb://127.0.0.1:27017/Account-ponshop",
+    {useNewUrlParser:true}
+)
+const db = mongoose.connection;
+db.once("open",()=>{
+    console.log("Succses connect ke monggose")
+})
  
 
 // static files 
 
-app.use(express.static('public'));
+app.use("/public",express.static(__dirname + '/public'));
  
 
 // routes 
-const auth = require('./routes/auth'); 
+app.get('/login', async (req,res)=>{
+    res.render('pages/Login');
+});
 
-app.use('/auth', auth); 
+app.post('/login', async (req,res)=>{
+
+const username = req.body.username;
+const password = req.body.password;
+
+acc.find({"username": username, "password": password}).exec((error,data)=>{
+    if(error) console.log(JSON.stringify(error));
+    if(data){
+        console.log("Find: "+JSON.stringify(data));
+        req.session.user = username;
+        res.render('pages/account')
+    }
+});
+});
+app.get('/account', async (req,res)=>{
+res.render('pages/Account');
+});
+
+app.get('/register',async(req,res)=>{
+res.render('pages/Login',{layout:false});
+});
+
+app.post('/register',async(req,res)=>{
+const username = req.body.username;
+const email = req.body.email;
+const password = req.body.password;
+const reppass = req.body.reppass;
+const dateof = req.body.dateof;
+const gender = req.body.gender;
+const address = req.body.address;
+const mobnum = req.body.mobnum;
+
+var acc_insert = new acc({
+    username : username,
+    email : email,
+    password : password,
+    reppass : reppass,
+    dateof : dateof,
+    gender : gender,
+    address : address,
+    mobnum : mobnum,
+})
+acc_insert.save((err,product) => {
+    if (err) console.log(err);
+    console.log(JSON.stringify(product))
+    res.redirect('/login');
+});
+});
+
+app.get('/logout',async(req,res)=>{
+req.session.destroy();
+res.redirect('/login');
+});
+module.exports = app;
 
 // start server on port 3000 
 
--- Bahan WEB/routes/auth.js
+++ Bahan WEB/routes/auth.js
@@ -1,4 +1,4 @@
-const { json } = require('body-parser');
+const body = require('body-parser');
 const { error } = require('console');
 const express = require('express');
 const session = require('express-session');
@@ -15,58 +15,5 @@ db.once("open",()=>{
 })
 const account = require('../models/User')
 const Router = express.Router();
+Router.use(express.json());
 
-Router.get('/login', async (req,res)=>{
-        res.render('pages/Login');
-});
-
-Router.post('/login', async (req,res)=>{
-    
-    const username = req.body.username;
-    const password = req.body.password;
-
-    acc.find({"username": username, "password": password}).exec((error,data)=>{
-        if(error) console.log(j son.stringify(error));
-        if(data){
-            console.log("Find: "+json.stringify(data));
-            req.session.user = username;
-            res.redirect('/');
-        }
-    });
-});
-Router.get('/register',async(req,res)=>{
-    res.render('pages/Login',{layout:false});
-});
-
-Router.post('/register',async(req,res)=>{
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const reppass = req.body.reppass;
-    const dateof = req.body.dateof;
-    const gender = req.body.gender;
-    const address = req.body.address;
-    const mobnum = req.body.mobnum;
-    
-    var acc_insert = new acc({
-        username : username,
-        email : email,
-        password : password,
-        reppass : reppass,
-        dateof : dateof,
-        gender : gender,
-        address : address,
-        mobnum : mobnum,
-    })
-    acc_insert.save((err,product) => {
-        if (err) console.log(err);
-        console.log(JSON.stringify(product))
-        res.redirect('/login');
-    });
-});
-
-Router.get('/logout',async(req,res)=>{
-    req.session.destroy();
-    res.redirect('/login');
-});
-module.exports = Router;
