7751f94dcb01be5371aa04001261786f97b37068
ivnnj <abcdef33yk@gmail.com>
2020-12-10 03:22:39 +0700

    update

---
+++ Bahan WEB/public/assets/js/index.js
@@ -0,0 +1,37 @@
+const express = require('express'); 
+const router = express.Router();
+const bodyParser = require('body-parser'); 
+
+ 
+
+const app = express(); 
+
+router.use(express.json());
+
+// set the view engine to ejs 
+
+app.set('view engine', 'ejs'); 
+
+ 
+
+// body-parser to parse request body 
+
+app.use(bodyParser.urlencoded()); 
+
+ 
+
+// static files 
+
+app.use(express.static('public'));
+ 
+
+// routes 
+const auth = require('./routes/auth'); 
+
+app.use('/auth', auth); 
+
+// start server on port 3000 
+
+app.listen(3000); 
+
+console.log('Server runs at port 3000...'); 
