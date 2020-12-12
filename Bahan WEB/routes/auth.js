329400f5618a9d149547b3d94c9c024f3b94120c
StevenTjayadi <steven.535180085@stu.untar.ac.id>
2020-12-12 14:32:30 +0700

    Edit routes database dan schema

--- Bahan WEB/routes/auth.js
+++ Bahan WEB/routes/auth.js
@@ -3,17 +3,18 @@ const { error } = require('console');
 const express = require('express');
 const session = require('express-session');
 const mongoose = require("mongoose");
-const acc = require('../models/user');
 
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/Account-ponshop",
+    "mongodb://127.0.0.1:27017/Men_Category_Suit",
     {useNewUrlParser:true}
 )
 const db = mongoose.connection;
 db.once("open",()=>{
-    console.log("Succses connect ke monggose")
+    console.log("Success connect ke monggose")
 })
-const account = require('../models/User')
+const Database = require('../models/database')
 const Router = express.Router();
 Router.use(express.json());
+
+
 
